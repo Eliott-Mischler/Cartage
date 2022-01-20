f33097917a30cbd4802506dffde1bd612f0dea18
Eliott-Mischler <97945839+Eliott-Mischler@users.noreply.github.com>
2022-01-20 14:01:40 +0100

    update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -79,8 +79,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'cartage',
-        'USER': 'root',
-        'PASSWORD': 'Darcoub9192631770',
+        'USER': 'formateur',
+        'PASSWORD': 'formateur',
         'HOST': '127.0.0.1',
         'PORT': '3306'
     }
